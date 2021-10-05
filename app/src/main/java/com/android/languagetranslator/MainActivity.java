b0e6ee6a332c3a0778bd3b3c611402b53cce5a62
Riyaaz <rahmatullaha845@gmail.com>
2021-10-05 15:43:04 +0530

    LT-0.0.2

--- app/src/main/java/com/android/languagetranslator/MainActivity.java
+++ app/src/main/java/com/android/languagetranslator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.languagetranslator;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +15,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 import java.util.ArrayList;
@@ -111,7 +117,21 @@ public class MainActivity extends AppCompatActivity {
                 .setSourceLanguage(from_language_code)
                 .setTargetLanguage(to_language_code)
                 .build();
-
+        FirebaseTranslator translator= FirebaseNaturalLanguage.getInstance()
+                .getTranslator(options);
+        FirebaseModelDownloadConditions conditions=new FirebaseModelDownloadConditions.Builder().build();
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                result.setText("Translating language...");
+                translator.translate(text).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        result.setText(s);
+                    }
+                }).addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Error due to : - "+e.getMessage(), Toast.LENGTH_SHORT).show());
+            }
+        }).addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Can not able to download due to :- "+e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
     private int get_language_code(String s) {
